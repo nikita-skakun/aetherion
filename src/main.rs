529232fd5d3158fd8609763aa8a92c30e948855d
Nikita Skakun <nikita@skakun-family.com>
2023-04-01 11:40:41 -0700

    Moved spectator_camera code into its own file.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use bevy::{input::mouse::MouseMotion, prelude::*, window::CursorGrabMode};
+mod spectator_camera;
 
-const CAMERA_MOVE_SPEED: f32 = 5.0;
-const CAMERA_VIEW_SPEED: f32 = 3.0;
+use bevy::{prelude::*, window::CursorGrabMode};
+use spectator_camera::*;
 
 /// set up a simple 3D scene
 fn setup(
@@ -31,40 +31,6 @@ fn setup(
         transform: Transform::from_xyz(0.0, 2.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
-}
-
-fn move_camera(
-    time: Res<Time>,
-    keyboard_input: Res<Input<KeyCode>>,
-    mut motion_evr: EventReader<MouseMotion>,
-    mut query: Query<&mut Transform, With<Camera3d>>,
-) {
-    for mut transform in query.iter_mut() {
-        let rotation = transform.rotation;
-        if keyboard_input.pressed(KeyCode::W) {
-            transform.translation +=
-                rotation * Vec3::new(0.0, 0.0, -1.0) * time.delta_seconds() * CAMERA_MOVE_SPEED;
-        }
-        if keyboard_input.pressed(KeyCode::S) {
-            transform.translation +=
-                rotation * Vec3::new(0.0, 0.0, 1.0) * time.delta_seconds() * CAMERA_MOVE_SPEED;
-        }
-        if keyboard_input.pressed(KeyCode::A) {
-            transform.translation +=
-                rotation * Vec3::new(-1.0, 0.0, 0.0) * time.delta_seconds() * CAMERA_MOVE_SPEED;
-        }
-        if keyboard_input.pressed(KeyCode::D) {
-            transform.translation +=
-                rotation * Vec3::new(1.0, 0.0, 0.0) * time.delta_seconds() * CAMERA_MOVE_SPEED;
-        }
-        let (mut delta_x, mut delta_y) = (0.0, 0.0);
-        for ev in motion_evr.iter() {
-            delta_x += ev.delta.x;
-            delta_y += ev.delta.y;
-        }
-        transform.rotate(Quat::from_rotation_y(-delta_x * CAMERA_VIEW_SPEED * 0.001));
-        transform.rotate_local(Quat::from_rotation_x(-delta_y * CAMERA_VIEW_SPEED * 0.001));
-    }
 }
 
 fn toggle_cursor_lock(mut windows: Query<&mut Window>) {
---
+++ src/spectator_camera.rs
@@ -0,0 +1,38 @@
+pub use bevy::{input::mouse::MouseMotion, prelude::*};
+
+const CAMERA_MOVE_SPEED: f32 = 5.0;
+const CAMERA_VIEW_SPEED: f32 = 3.0;
+
+pub fn move_camera(
+    time: Res<Time>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut motion_evr: EventReader<MouseMotion>,
+    mut query: Query<&mut Transform, With<Camera3d>>,
+) {
+    for mut transform in query.iter_mut() {
+        let rotation = transform.rotation;
+        if keyboard_input.pressed(KeyCode::W) {
+            transform.translation +=
+                rotation * Vec3::new(0.0, 0.0, -1.0) * time.delta_seconds() * CAMERA_MOVE_SPEED;
+        }
+        if keyboard_input.pressed(KeyCode::S) {
+            transform.translation +=
+                rotation * Vec3::new(0.0, 0.0, 1.0) * time.delta_seconds() * CAMERA_MOVE_SPEED;
+        }
+        if keyboard_input.pressed(KeyCode::A) {
+            transform.translation +=
+                rotation * Vec3::new(-1.0, 0.0, 0.0) * time.delta_seconds() * CAMERA_MOVE_SPEED;
+        }
+        if keyboard_input.pressed(KeyCode::D) {
+            transform.translation +=
+                rotation * Vec3::new(1.0, 0.0, 0.0) * time.delta_seconds() * CAMERA_MOVE_SPEED;
+        }
+        let (mut delta_x, mut delta_y) = (0.0, 0.0);
+        for ev in motion_evr.iter() {
+            delta_x += ev.delta.x;
+            delta_y += ev.delta.y;
+        }
+        transform.rotate(Quat::from_rotation_y(-delta_x * CAMERA_VIEW_SPEED * 0.001));
+        transform.rotate_local(Quat::from_rotation_x(-delta_y * CAMERA_VIEW_SPEED * 0.001));
+    }
+}
