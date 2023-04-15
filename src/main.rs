c743e4ba5f9d72a279d91f4f19f9a46a2572e211
Nikita Skakun <nikita@skakun-family.com>
2023-04-15 01:02:14 -0700

    Began working on UI for an escape menu.

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,20 @@ use bevy::{
 use leafwing_input_manager::{prelude::InputManagerPlugin, InputManagerBundle};
 use spectator_camera::*;
 
+#[derive(Component)]
+struct EscapeMenuTag {
+    visible: bool,
+}
+
+#[derive(Resource)]
+struct CursorLockState(bool);
+
 /// set up a simple 3D scene
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
     // cube
     commands.spawn(PbrBundle {
@@ -42,25 +51,86 @@ fn setup(
             ..default()
         },
     });
+
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                flex_direction: FlexDirection::ColumnReverse,
+                justify_content: JustifyContent::Center,
+                display: Display::None,
+                align_items: AlignItems::Center,
+                ..Default::default()
+            },
+            background_color: Color::NONE.into(),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        size: Size::new(Val::Px(200.0), Val::Px(100.0)),
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        ..Default::default()
+                    },
+                    background_color: Color::rgb(0.2, 0.2, 0.2).into(),
+                    ..Default::default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(
+                        TextBundle::from_section(
+                            "Escape Menu",
+                            TextStyle {
+                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                font_size: 30.0,
+                                color: Color::WHITE,
+                            },
+                        )
+                        .with_style(Style {
+                            margin: UiRect::all(Val::Px(5.)),
+                            ..Default::default()
+                        }),
+                    );
+                });
+        })
+        .insert(EscapeMenuTag { visible: false });
 }
 
-fn toggle_cursor_lock(mut windows: Query<&mut Window>) {
+fn set_cursor_lock(mut windows: Query<&mut Window>, cursor_lock_state: Res<CursorLockState>) {
     let mut window = windows.single_mut();
-    window.cursor.visible = !window.cursor.visible;
-    window.cursor.grab_mode = match window.cursor.grab_mode {
-        CursorGrabMode::None => CursorGrabMode::Locked,
-        CursorGrabMode::Locked | CursorGrabMode::Confined => CursorGrabMode::None,
-    };
+    if cursor_lock_state.0 {
+        window.cursor.visible = false;
+        window.cursor.grab_mode = CursorGrabMode::Locked;
+    } else {
+        window.cursor.visible = true;
+        window.cursor.grab_mode = CursorGrabMode::Confined;
+    }
 }
 
-fn escape_menu(keyboard_input: Res<Input<KeyCode>>, windows: Query<&mut Window>) {
+fn escape_menu(
+    keyboard_input: Res<Input<KeyCode>>,
+    windows: Query<&mut Window>,
+    mut escape_menu_tag: Query<(&EscapeMenuTag, &mut Style)>,
+    mut cursor_lock_state: ResMut<CursorLockState>,
+) {
     if keyboard_input.just_pressed(KeyCode::Escape) {
-        toggle_cursor_lock(windows);
+        for (_, mut style) in escape_menu_tag.iter_mut() {
+            if let Display::None = style.display {
+                style.display = Display::Flex;
+                cursor_lock_state.0 = false;
+            } else {
+                style.display = Display::None;
+                cursor_lock_state.0 = true;
+            }
+        }
+        set_cursor_lock(windows, cursor_lock_state.into());
     }
 }
 
 fn main() {
     App::new()
+        .insert_resource(CursorLockState(true))
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Aetherion".into(),
@@ -73,7 +143,7 @@ fn main() {
         }))
         .add_plugin(InputManagerPlugin::<input::Action>::default())
         .add_startup_system(setup)
-        .add_startup_system(toggle_cursor_lock)
+        .add_startup_system(set_cursor_lock)
         .add_system(move_camera)
         .add_system(escape_menu)
         .run();
